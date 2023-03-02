6e4772b01a89756cecbcf6f29166bf71c7c6ba23
lukkas99 <lmoxavier@gmail.com>
2023-03-02 16:31:18 -0300

    primeiro commit somadois.

---
+++ Unidade01/src/SomarDoisNum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class SomarDoisNum {
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String args[]){
+		//Declaracao de variaveis
+		int num1, num2, soma;
+		//Leituras
+		System.out.println("Digite um numero");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro numero");
+		num2 = sc.nextInt();
+		//Somar
+		soma = num1 + num2;
+		//Mostrar na tela
+		System.out.println("Soma:" + soma);
+		
+		}
+
+	@Override
+	public String toString() {
+		return "SomarDoisNum []";
+	}
+
+	public static Scanner getSc() {
+		return sc;
+	}
+
+	public static void setSc(Scanner sc) {
+		SomarDoisNum.sc = sc;
+	}
+}
+
